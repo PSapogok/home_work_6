9619c5cd00a4bcfb04f777015b868ef4e9a35fec
Павел <71460836+PSapogok@users.noreply.github.com>
2020-11-03 16:50:10 +0300

    Add files via upload

---
+++ dz6_2.py
@@ -0,0 +1,12 @@
+spisok_prochitan={'vlastik', 'hobbit', 'zona51'}
+spisok_obyz={'vlastik', 'hobbit'}
+for i in spisok_prochitan:
+    for j in spisok_obyz:
+        if spisok_prochitan[i] == spisok_obyz[j]:
+            print(spisok_prochitan[i])
+           
+            
+            
+                    
+
+                
